6e215f94d97649b4916fa73870f0dab3918da130
rafa <rmxvgit@gmail.com>
2025-02-18 19:40:42 -0300

    eu gosto de batata

--- pull.py
+++ pull.py
@@ -1,4 +1,6 @@
 import ftplib as ftp
+import csv
+from dbfread import DBF
 import sys
 import os
 
@@ -48,7 +50,7 @@ def sia_pull(estado: str, data_inicio: dict[str, int], data_fim: dict[str, int])
         print("Conversão para dbf...")
         os.system(f"./blast-dbf {fileName} {fileName[:-4]}.dbf" )
         print("Conversão para csv...")
-        os.system(f"python3 ./dbf2csv/dbf2csv/main.py {fileName[:-4]}.dbf {fileName[:-4]}.csv")
+        dbf_to_csv(fileName[:-4] + ".dbf")
 
 def is_date_less(date1: dict[str, int], date2: dict[str, int]) -> bool:
     return date1['year'] < date2['year'] or (date1['year'] == date2['year'] and date1['month'] < date2['month'])
@@ -86,7 +88,7 @@ def sih_pull(estado: str, data_inicio: dict[str, int], data_fim: dict[str, int])
         print("Conversão para dbf...")
         os.system(f"./blast-dbf {fileName} {fileName[:-4]}.dbf" )
         print("Conversão para csv...")
-        os.system(f"python3 ./dbf2csv/dbf2csv/main.py {fileName[:-4]}.dbf {fileName[:-4]}.csv")
+        dbf_to_csv(fileName[:-4] + ".dbf")
 
 def find_sih_files_of_interest(estado: str, data_inicio: dict[str, int], data_fim: dict[str, int]) -> list[str]:
     files = []
@@ -140,5 +142,13 @@ def dowload_from_ftp(ftp_server: str, remote_path: str, local_dir: str):
         print("Erro ao fazer download", remote_path)
         return
 
+def dbf_to_csv(dbf_table_pth):#Input a dbf, output a csv, same name, same path, except extension
+    csv_fn = dbf_table_pth[:-4]+ ".csv" #Set the csv file name
+    table = DBF(dbf_table_pth)# table variable is a DBF object
+    with open(csv_fn, 'w', newline = '') as f:# create a csv file, fill it with dbf content
+        writer = csv.writer(f)
+        writer.writerow(table.field_names)# write the column name
+        for record in table:# write the rows
+            writer.writerow(list(record.values()))
 
 main()
